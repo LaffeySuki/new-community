1104d0e7775c98d31ded4916cb30da4e179a75ec
LaffeySuki <1269818791@qq.com>
2022-05-08 21:57:10 +0800

    ssl problem

--- src/main/java/life/majiang/community/provider/GithubProvider.java
+++ src/main/java/life/majiang/community/provider/GithubProvider.java
@@ -13,7 +13,7 @@ import java.io.IOException;
 public class GithubProvider {
     public String getAccessToken(AccessTokenDTO accessTokenDTO)
     {
-        String url = "https://github.com/login/oauth/access_token";
+        String url = "http://github.com/login/oauth/access_token";
         MediaType mediaType = MediaType.get("application/json;charset=utf-8");
         OkHttpClient client = new OkHttpClient();
         RequestBody body = RequestBody.create(mediaType,JSON.toJSONString(accessTokenDTO));
@@ -21,11 +21,12 @@ public class GithubProvider {
                 .url(url)
                 .post(body)
                 .build();
-        try(Response response = client.newCall(request).execute())
+        try
         {
+            Response response = client.newCall(request).execute();
             String s = response.body().string();
-            System.out.println(s);
-            return s;
+            String token = s.split("&")[0].split("=")[1];
+            return token;
         }
         catch (Exception e)
         {
@@ -34,11 +35,12 @@ public class GithubProvider {
         return null;
     }
 
-    public GithubUser getGithubUser(String accessToken)
+    public GithubUser getUser(String accessToken)
     {
         OkHttpClient client = new OkHttpClient();
         Request request = new Request.Builder()
-                .url("http://api.github.com/user?access_token" + accessToken)
+                .url("https://api.github.com/user")
+                .header("Authorization", "token " + accessToken)
                 .build();
         try {
             Response response = client.newCall(request).execute();
