674db2c3fdb5e2fb6a6a41c1f43c61237fca7ca2
LaffeySuki <1269818791@qq.com>
2022-05-20 18:51:16 +0800

    add mapper commit

--- src/main/java/life/majiang/community/dto/GithubUser.java
+++ src/main/java/life/majiang/community/dto/GithubUser.java
@@ -27,5 +27,12 @@ public class GithubUser {
         this.bio = bio;
     }
 
-
+    @Override
+    public String toString() {
+        return "GithubUser{" +
+                "name='" + name + '\'' +
+                ", id='" + id + '\'' +
+                ", bio='" + bio + '\'' +
+                '}';
+    }
 }
